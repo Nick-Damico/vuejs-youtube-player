3417c62bb281416d8cb8062f2e3bb051a8cf771b
Nick-Damico <nickalan82@icloud.com>
2019-07-16 20:17:19 -0400

    Setup props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
+<!-- Directives can also be declared as v-on:termChange="onTermChange" -->
+<!-- Directive to bind props: v-bind:videos="videos" -->
 <template>
   <div>
     VueTube Browser
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
+// import dependencies
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 // Define a env variable in .env file with API KEY.
 const API_KEY = process.env.VUE_APP_SECRET;
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return {
+      videos: []
+    };
   },
   methods: {
     onTermChange: function(searchTerm) {
@@ -22,10 +33,12 @@ export default {
           key: API_KEY,
           type: 'video',
           part: 'snippet',
-          q: 'searchTerm'
+          q: searchTerm
         }
       })
-      .then(response => console.log(response))
+      .then(response => {
+        this.videos = response.data.items
+      })
       .catch(error => console.log(error))
     }
   }
---
+++ src/components/VideoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    Video List
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  props: {
+    videos: Array
+  }
+};
+</script>
+
+<style>
+</style>
