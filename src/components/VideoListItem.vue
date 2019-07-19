ae0108b9e8e099e9ea9a4e69952603c421358b66
Nick-Damico <nickalan82@icloud.com>
2019-07-18 21:31:22 -0400

    Add computed function and styling

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,12 +1,41 @@
 <template lang="html">
-  <li class="list-group-item">
-    {{ video.snippet.title }}
+  <li class="list-group-item media">
+    <img
+      class="mr-3"
+      :src="thumbnailUrl"
+      :alt="description"
+    />
+    <div class="media-body">
+      {{ title }}
+    </div>
   </li>
 </template>
 
 <script>
 export default {
   name: 'VideoListItem',
-  props: ['video']
+  props: ['video'],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.url;
+    },
+    title() {
+      return this.title.snippet.title;
+    },
+    description() {
+      return this.video.snippet.description;
+    }
+  }
 }
 </script>
+
+<style scoped>
+  li {
+    display: flex;
+    cursor: pointer;
+    transition: background-color .2s ease-in;
+  }
+  li:hover {
+    background-color: #eee;
+  }
+</style>
